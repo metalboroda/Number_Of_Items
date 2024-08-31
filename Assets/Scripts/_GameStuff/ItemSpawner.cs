c59ae3aad76b35300f742b53d6164cc607981d4e
metalboroda <89025071+metalboroda@users.noreply.github.com>
2024-08-31 10:47:35 +0300

    Finished

--- Assets/Scripts/_GameStuff/ItemSpawner.cs
+++ Assets/Scripts/_GameStuff/ItemSpawner.cs
@@ -4,15 +4,15 @@ namespace Assets.Scripts._GameStuff
 {
   public class ItemSpawner : MonoBehaviour
   {
+    [SerializeField] private float _sphereRadius = 5f;
+    [SerializeField] private Vector3 _sphereCenter = Vector3.zero;
+
+    [Header("")]
     [SerializeField] private SpawnerItem[] _items;
 
     public SpawnerItem[] Items => _items;
 
-    private CapsuleCollider _capsuleCollider;
-
-    private void Awake() {
-      _capsuleCollider = GetComponent<CapsuleCollider>();
-
+    private void Start() {
       SpawnItems();
     }
 
@@ -20,22 +20,24 @@ namespace Assets.Scripts._GameStuff
       foreach (var item in _items) {
         for (int i = 0; i < item.Amount; i++) {
           Vector3 randomPosition = GetRandomPositionInsideSphere();
-
           float randomVector = Random.Range(-360f, 360f);
           Quaternion randomRotation = Quaternion.Euler(randomVector, randomVector, randomVector);
 
           Instantiate(item.BoxItemHandler, randomPosition, randomRotation, transform);
         }
       }
-
-      _capsuleCollider.enabled = false;
     }
 
     private Vector3 GetRandomPositionInsideSphere() {
-      Vector3 randomDirection = Random.insideUnitSphere;
-      Vector3 randomPosition = _capsuleCollider.center + randomDirection * _capsuleCollider.radius;
+      Vector3 randomDirection = Random.insideUnitSphere.normalized;
+      Vector3 randomPosition = _sphereCenter + randomDirection * Random.Range(0, _sphereRadius);
 
       return transform.TransformPoint(randomPosition);
+    }
+
+    private void OnDrawGizmos() {
+      Gizmos.color = Color.green;
+      Gizmos.DrawWireSphere(transform.TransformPoint(_sphereCenter), _sphereRadius);
     }
   }
 }
